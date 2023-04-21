32ae706dc5c240bb08209cd42a1d3b64ebcd6970
Michaël Vanderheyden <michael.vanderheyden@atos.net>
2023-04-21 20:07:59 +0000

    test: setup tooling

---
+++ src/index.spec.mjs
@@ -0,0 +1,8 @@
+import { describe, it } from 'vitest';
+import { init, sync } from './index.mjs';
+
+
+describe('test hw2energyid', function() {
+    it('test hw2energyid.init', function(done) {
+    })
+})
