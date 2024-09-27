912b8d42c2952b91c03ed9e0343286321116d235
Michaël Vanderheyden <hello@th3s4mur41.me>
2024-09-27 16:36:46 +0200

    style: run biome before prettier
    Avoid a bug where biomd does not run, when called the other way around

--- src/homewizard/device.spec.mjs
+++ src/homewizard/device.spec.mjs
@@ -1,98 +1,98 @@
 // src/homewizard/device.test.mjs
-import { describe, it, expect, beforeEach, vi } from 'vitest';
-import { Device } from './device.mjs';
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { Device } from "./device.mjs";
 
 globalThis.fetch = vi.fn();
 
-describe('Device', () => {
-  const mockAddress = '192.168.1.1';
-  const mockOffset = 10;
-  const mockData = {
-    product_name: 'HomeWizard',
-    serial: '123456789',
-    firmware_version: '1.0.0',
-    api_version: 'v1',
-  };
-  const mockApiResponse = {
-    data: 'mockData',
-  };
+describe("Device", () => {
+	const mockAddress = "192.168.1.1";
+	const mockOffset = 10;
+	const mockData = {
+		product_name: "HomeWizard",
+		serial: "123456789",
+		firmware_version: "1.0.0",
+		api_version: "v1",
+	};
+	const mockApiResponse = {
+		data: "mockData",
+	};
 
-  beforeEach(() => {
-    fetch.mockReset();
-  });
+	beforeEach(() => {
+		fetch.mockReset();
+	});
 
-  it('should initialize successfully with valid data', async () => {
-    fetch.mockResolvedValueOnce({
-      json: async () => mockData,
-    });
+	it("should initialize successfully with valid data", async () => {
+		fetch.mockResolvedValueOnce({
+			json: async () => mockData,
+		});
 
-    const device = await Device.init(mockAddress, mockOffset);
+		const device = await Device.init(mockAddress, mockOffset);
 
-    expect(device).toBeInstanceOf(Device);
-    expect(device.offset).toBe(mockOffset);
-    expect(device.data).toEqual({});
-  });
+		expect(device).toBeInstanceOf(Device);
+		expect(device.offset).toBe(mockOffset);
+		expect(device.data).toEqual({});
+	});
 
-  it('should throw an error if initialization fails', async () => {
-    fetch.mockRejectedValueOnce(new Error('Network Error'));
+	it("should throw an error if initialization fails", async () => {
+		fetch.mockRejectedValueOnce(new Error("Network Error"));
 
-    await expect(Device.init(mockAddress, mockOffset)).rejects.toThrow('Cannot initialize 192.168.1.1');
-  });
+		await expect(Device.init(mockAddress, mockOffset)).rejects.toThrow("Cannot initialize 192.168.1.1");
+	});
 
-  it('should update data successfully', async () => {
-    fetch.mockResolvedValueOnce({
-      json: async () => mockData,
-    });
+	it("should update data successfully", async () => {
+		fetch.mockResolvedValueOnce({
+			json: async () => mockData,
+		});
 
-    const device = await Device.init(mockAddress, mockOffset);
+		const device = await Device.init(mockAddress, mockOffset);
 
-    fetch.mockResolvedValueOnce({
-      json: async () => mockApiResponse,
-    });
+		fetch.mockResolvedValueOnce({
+			json: async () => mockApiResponse,
+		});
 
-    const data = await device.update();
+		const data = await device.update();
 
-    expect(data).toEqual(mockApiResponse);
-    expect(device.data).toEqual(mockApiResponse);
-  });
+		expect(data).toEqual(mockApiResponse);
+		expect(device.data).toEqual(mockApiResponse);
+	});
 
-  it('should handle data update failure', async () => {
-    fetch.mockResolvedValueOnce({
-      json: async () => mockData,
-    });
+	it("should handle data update failure", async () => {
+		fetch.mockResolvedValueOnce({
+			json: async () => mockData,
+		});
 
-    const device = await Device.init(mockAddress, mockOffset);
+		const device = await Device.init(mockAddress, mockOffset);
 
-    fetch.mockRejectedValueOnce(new Error('Network Error'));
+		fetch.mockRejectedValueOnce(new Error("Network Error"));
 
-    await device.update();
+		await device.update();
 
-    expect(device.data).toEqual({});
-  });
+		expect(device.data).toEqual({});
+	});
 
-  it('should return correct offset', async () => {
-    fetch.mockResolvedValueOnce({
-      json: async () => mockData,
-    });
+	it("should return correct offset", async () => {
+		fetch.mockResolvedValueOnce({
+			json: async () => mockData,
+		});
 
-    const device = await Device.init(mockAddress, mockOffset);
+		const device = await Device.init(mockAddress, mockOffset);
 
-    expect(device.offset).toBe(mockOffset);
-  });
+		expect(device.offset).toBe(mockOffset);
+	});
 
-  it('should return correct data', async () => {
-    fetch.mockResolvedValueOnce({
-      json: async () => mockData,
-    });
+	it("should return correct data", async () => {
+		fetch.mockResolvedValueOnce({
+			json: async () => mockData,
+		});
 
-    const device = await Device.init(mockAddress, mockOffset);
+		const device = await Device.init(mockAddress, mockOffset);
 
-    fetch.mockResolvedValueOnce({
-      json: async () => mockApiResponse,
-    });
+		fetch.mockResolvedValueOnce({
+			json: async () => mockApiResponse,
+		});
 
-    await device.update();
+		await device.update();
 
-    expect(device.data).toEqual(mockApiResponse);
-  });
+		expect(device.data).toEqual(mockApiResponse);
+	});
 });
--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vitest/config'
-
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    reporters: ['default'],
-    coverage: {
-      provider: 'v8',
-      include: ['src/**']
-    },
-  },
-})
+	test: {
+		reporters: ["default"],
+		coverage: {
+			provider: "v8",
+			include: ["src/**"],
+		},
+	},
+});
